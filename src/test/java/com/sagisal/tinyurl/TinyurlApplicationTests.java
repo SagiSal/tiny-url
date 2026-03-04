1a772773481e58169a4845a8ef4dd0deb8c960d0
Sagi <sagisalah29@gmail.com>
2026-03-04 18:35:44 +0200

    Initial import of project

---
+++ src/test/java/com/sagisal/tinyurl/TinyurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagisal.tinyurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
